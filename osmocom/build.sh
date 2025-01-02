2bb1e377d48c9056f78913996a48af0314692101
Nikolay <nalussar@gmail.com>
2025-01-02 19:02:16 +0300

    Removed git clone

--- osmocom/build.sh
+++ osmocom/build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-echo "Insalling liburing"
-git clone https://gitea.osmocom.org/sdr/libosmo-dsp
 cd libosmo-dsp/
 autoreconf -i
 ./configure
 make
 make install
 cd ..
-git clone https://gitea.osmocom.org/phone-side/osmocom-bb -b fixeria/trx trx
 cd trx/src
 echo "CFLAGS += -DCONFIG_TX_ENABLE" >> target/firmware/Makefile
 make HOST_layer23_CONFARGS=--enable-transceiver
